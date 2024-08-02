e76bf131257da75608326dd389ad3ae95a09256d
Ngeyen <ngeyen@cmu.edu>
2024-08-02 13:17:29 +0200

    fixed login for admin and patient

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import UserManager.models.Admin;
 import UserManager.models.Patient;
+import UserManager.models.UserRole;
 import UserManager.services.UserService;
 
 import java.time.LocalDate;
@@ -13,25 +15,25 @@ public class Main {
         System.out.println("Welcome to the Life prognosis management System");
 
         while (true) {
-            System.out.println("1. Admin - Initiate Patient Registration");
-            System.out.println("2. Patient - Complete Registration");
-            System.out.println("3. User - Login");
-            System.out.println("4. Exit");
+
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+
+
+            System.out.println("3. Exit");
 
             int choice = scanner.nextInt();
             scanner.nextLine(); // Consume newline
 
             switch (choice) {
+
                 case 1:
-                    initiatePatientRegistration();
-                    break;
-                case 2:
                     completePatientRegistration();
                     break;
-                case 3:
+                case 2:
                     login();
                     break;
-                case 4:
+                case 3:
                     System.out.println("Exiting...");
                     return;
                 default:
@@ -48,6 +50,24 @@ public class Main {
             System.out.println("Registration initiated. Share this UUID with the patient: " + uuid);
         } else {
             System.out.println("Failed to initiate registration. Please try again.");
+        }
+    }
+    private  static  void createAdmin(){
+        System.out.print("Enter first name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter last name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Enter email: ");
+        String email = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        Admin admin = new Admin(firstName, lastName, email, password);
+        boolean success = userService.createUser(admin);
+        if (success) {
+            System.out.println("Admin added created.");
+        }else {
+            System.out.println("Failed to create Admin. Please try again.");
         }
     }
 
@@ -101,9 +121,51 @@ public class Main {
         System.out.print("Enter password: ");
         String password = scanner.nextLine();
 
-        boolean success = userService.verifyLoginCredentials(email, password, null);
-        if (success) {
+        UserRole role = userService.verifyLoginCredentials(email, password);
+        if (role !=null) {
             System.out.println("Login successful.");
+            System.out.println(role + "\n========================");
+            if (role == UserRole.ADMIN) {
+                System.out.println("Admin Menu.");
+
+                System.out.println("1. Create new patient");
+                System.out.println("2. Add an Admin");
+                System.out.println("3. Download Patient Info");
+
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
+
+                switch (choice) {
+                    case 1:
+                        initiatePatientRegistration();
+                        break;
+                    case 2:
+                        createAdmin();
+                        break;
+                    case 4:
+                        System.out.println("Exiting...");
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            }else {
+                System.out.println("User logged in.");
+                System.out.println("1. Update my Profile");
+                System.out.println("2. Exit");
+                int choice = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
+
+                switch (choice) {
+                    case 1:
+                        completePatientRegistration();
+                        break;
+                    case 2:
+                        System.out.println("Exiting...");
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            }
         } else {
             System.out.println("Login failed. Please check your credentials.");
         }
