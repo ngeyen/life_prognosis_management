2edfc132d3bc9bc022ffd9691778d7cbc842b93d
Ngeyen <ngeyen@cmu.edu>
2024-08-07 10:56:52 +0200

    survival rate progress

---
+++ src/statistics/services/SurvialRate.java
@@ -0,0 +1,18 @@
+package statistics.services;
+
+import UserManager.models.User;
+
+public class SurvialRate {
+
+
+    SurvialRate() {
+
+    }
+    SurvialRate(User patient) {
+
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
