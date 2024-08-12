a97f96d336aa24c5d5810e1766e06eb37bb55b0d
Ngeyen <ngeyen@cmu.edu>
2024-08-12 15:02:36 +0200

    added more env. variables

--- setup.sh
+++ setup.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-
+clear
 # Get the absolute path to the root directory
 ROOT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Set environment variables using relative paths
-export USER_MANAGER_SCRIPT="$ROOT_DIR/resources/scripts/user-manager.sh"
-export USER_STORE_PATH="$ROOT_DIR/resources/data/user-store.txt"
-export SURVIVAL_RATE_SCRIPT="$ROOT_DIR/resources/scripts/survival-rate.sh"
+export USER_MANAGER_SCRIPT="$ROOT_DIR/resources/scripts/user_manager.sh"
+export SURVIVAL_RATE_SCRIPT="$ROOT_DIR/resources/scripts/survival_rate.sh"
+export SEARCH_COUNTRIES_SCRIPT="$ROOT_DIR/resources/scripts/search_country.sh"
 export AUTH_SCRIPT="$ROOT_DIR/resources/scripts/auth.sh"
 export REGISTER_SCRIPT="$ROOT_DIR/resources/scripts/register.sh"
+
+export USER_STORE_PATH="$ROOT_DIR/resources/data/user-store.txt"
 export LIFE_EXPECTANCY_PATH="$ROOT_DIR/resources/data/life-expectancy.csv"
 export PIN_LENGTH=4  # Example of a non-path environment variable
+export PATIENT_DATA_PATH="$ROOT_DIR/downloads/patient_data.csv"
+
 
 # Make all relevant scripts executable
 chmod +x "$USER_MANAGER_SCRIPT"
 chmod +x "$SURVIVAL_RATE_SCRIPT"
 chmod +x "$AUTH_SCRIPT"
 chmod +x "$REGISTER_SCRIPT"
+chmod +x "$SEARCH_COUNTRIES_SCRIPT"
 
 echo "Environment variables set and scripts are now executable."
 
